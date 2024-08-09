af64dc1061f5cfbd262b5ddf573d6aa5e06ed75d
Upikk <96323919+Upikk@users.noreply.github.com>
2024-08-09 06:20:16 +0200

    Update buttonpagination.js

--- Events/ButtonPagination/buttonpagination.js
+++ Events/ButtonPagination/buttonpagination.js
@@ -9,12 +9,12 @@ const {
 
 const { readFileSync } = require("fs");
 
-const { LANGUAGE, PERMISSIONS } = require("../../config.json");
+const { LANGUAGE, PERMISSIONS, FIVEGUARD_RESOURCE_NAME } = require("../../config.json");
 
-let bansjson = require("../../bans.json");
+let bansjson = JSON.parse(readFileSync(`${GetResourcePath(FIVEGUARD_RESOURCE_NAME)}/bans.json`, { encoding: "utf8" }));
 
 global.RefreshBanlistEvents = function () {
-  bansjson = JSON.parse(readFileSync("./bans.json", { encoding: "utf8" }));
+  bansjson = JSON.parse(readFileSync(`${GetResourcePath(FIVEGUARD_RESOURCE_NAME)}/bans.json`, { encoding: "utf8" }));
 };
 
 module.exports = {
