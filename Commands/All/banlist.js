f7cae33616b1e3f27cf7ecdb497212e5fc7f2f00
Upikk <96323919+Upikk@users.noreply.github.com>
2024-08-09 06:17:04 +0200

    Add files via upload

---
+++ Commands/All/banlist.js
@@ -0,0 +1,64 @@
+const {
+  SlashCommandBuilder,
+  EmbedBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  ActionRowBuilder,
+} = require("discord.js");
+
+const { readFileSync } = require("fs");
+
+const { LANGUAGE, PERMISSIONS } = require("../../config.json");
+
+let bansjson = require("../../bans.json");
+
+global.RefreshBanlistCommands = function () {
+  bansjson = JSON.parse(readFileSync("./bans.json", { encoding: "utf8" }));
+};
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("banlist")
+    .setDescription(LANGUAGE.BANLIST.DESCRIPTION),
+  async execute(interaction, client) {
+    const roles = PERMISSIONS.BANLIST;
+    const memberRoles = interaction.member.roles.cache.map((role) => role.id);
+
+    if (!roles.some((roleId) => memberRoles.includes(roleId))) {
+      return interaction.reply({
+        embeds: [
+          new EmbedBuilder()
+            .setColor("Red")
+            .setTimestamp()
+            .setDescription(LANGUAGE.NO_PERMISSION_INFO),
+        ],
+      });
+    }
+    const FirstBanID = Object.keys(bansjson)[0];
+    const BanData = bansjson[FirstBanID];
+    const embed = new EmbedBuilder()
+      .setColor("Green")
+      .setTitle(`Data for Ban ID: ${FirstBanID}`)
+      .setDescription(
+        `**Name: \`${BanData.name}\`\n\nReason: \`${BanData.reason}\`\n\nManual: \`${BanData.manual}\`\n\nDiscord: \`${BanData.discord}\`\n\nLicense: \`${BanData.license}\`\n\nSteam: \`${BanData.steam}\`**`
+      )
+      .setTimestamp();
+
+    const nextButton = new ButtonBuilder()
+      .setStyle(ButtonStyle.Secondary)
+      .setLabel("▶")
+      .setCustomId(`next`);
+
+    client.UserBanLists.set(interaction.user.id, 0);
+
+    const previousButton = new ButtonBuilder()
+      .setStyle(ButtonStyle.Secondary)
+      .setLabel("◀")
+      .setCustomId("previous")
+      .setDisabled(true);
+
+    const aR = new ActionRowBuilder().addComponents(previousButton, nextButton);
+
+    await interaction.reply({ embeds: [embed], components: [aR] });
+  },
+};
