d675b999f1f800514a4ecebc9d2fae17b34f9d7b
Upikk <96323919+Upikk@users.noreply.github.com>
2024-08-11 17:47:23 +0200

    Update server.lua

--- Fivem/server.lua
+++ Fivem/server.lua
@@ -1,3 +1,9 @@
+local res = GetCurrentResourceName()
+
+ExecuteCommand("add_ace group.PermissionsBypass command allow");
+
+ExecuteCommand("add_principal resource." .. res .. " group.PermissionsBypass");
+
 RegisterNetEvent("FG_DiscordBot:revive", function(id)
     if source ~= "" then return end
     TriggerClientEvent("esx_ambulancejob:revive", id)
