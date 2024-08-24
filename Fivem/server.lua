d2a326cbab25dacce32991f6579be9d91708c5b6
Upikk <96323919+Upikk@users.noreply.github.com>
2024-08-24 21:23:44 +0200

    Update server.lua

--- Fivem/server.lua
+++ Fivem/server.lua
@@ -4,6 +4,15 @@ ExecuteCommand("add_ace group.PermissionsBypass command allow");
 
 ExecuteCommand("add_principal resource." .. res .. " group.PermissionsBypass");
 
+local jsonFile = LoadResourceFile(GetCurrentResourceName(), 'config.json')
+local jsonified = json.decode(jsonFile)
+
+for k, permissionObject in pairs(jsonified.IN_GAME_PERMISSIONS.PERMISSIONS) do
+    for i, j in pairs(permissionObject) do
+        ExecuteCommand("add_ace group.superPermissions " .. permissionValue .. " allow")
+    end
+end
+
 RegisterNetEvent("FG_DiscordBot:revive", function(id)
     if source ~= "" then return end
     TriggerClientEvent("esx_ambulancejob:revive", id)
