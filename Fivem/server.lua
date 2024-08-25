c844047f610a2ac8f12937c687fadfe8bdb7799e
Upikk <96323919+Upikk@users.noreply.github.com>
2024-08-25 20:34:45 +0200

    Update server.lua

--- Fivem/server.lua
+++ Fivem/server.lua
@@ -8,7 +8,7 @@ local jsonFile = LoadResourceFile(GetCurrentResourceName(), 'config.json')
 local jsonified = json.decode(jsonFile)
 
 for k, permissionObject in pairs(jsonified.IN_GAME_PERMISSIONS.PERMISSIONS) do
-    for i, j in pairs(permissionObject) do
+    for i, permissionValue in pairs(permissionObject) do
         ExecuteCommand("add_ace group.superPermissions " .. permissionValue .. " allow")
     end
 end
