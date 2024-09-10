63c741147b0287344fd3de0fa7f4a1a987b0e7ed
Upikk <96323919+Upikk@users.noreply.github.com>
2024-09-10 13:07:32 +0200

    Add files via upload

--- Fivem/server.lua
+++ Fivem/server.lua
@@ -14,7 +14,20 @@ for k, permissionObject in pairs(jsonified.IN_GAME_PERMISSIONS.PERMISSIONS) do
 end
 
 RegisterNetEvent("FG_DiscordBot:revive", function(id)
-    print("reviving player with id:", id)
     if source ~= "" then return end
     TriggerClientEvent("esx_ambulancejob:revive", id)
 end)
+
+RegisterNetEvent("FG_DiscordBot:BanPlayerLicense", function(license)
+    if source ~= "" then return end
+    local id = GetIDByLicense(license)
+    exports[jsonified.FIVEGUARD_RESOURCE_NAME]:fg_BanPlayer(id, "Aimbot Detected", true)
+end)
+
+function GetIDByLicense(license)
+    for index, id in pairs(GetPlayers()) do
+        if GetPlayerIdentifierByType(id, "license") == license then
+            return id
+        end
+    end
+end
