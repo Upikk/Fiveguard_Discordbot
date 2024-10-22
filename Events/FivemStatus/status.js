c08556dfe11d8cb33beeeae2bed6034a263c7045
Upikk <96323919+Upikk@users.noreply.github.com>
2024-10-22 19:55:08 +0200

    Update status.js

--- Events/FivemStatus/status.js
+++ Events/FivemStatus/status.js
@@ -1,6 +1,5 @@
 const { Client, ActivityType } = require("discord.js");
 const config = require("../../config.json");
-const fetch = require("node-fetch");
 
 module.exports = {
   name: "ready",
@@ -11,23 +10,11 @@ module.exports = {
   async execute(client) {
     if (config.SERVER_STATUS.ON) {
       setInterval(async () => {
-        try {
-          await fetch(
-            `http://127.0.0.1:${config.SERVER_STATUS.SERVER_PORT}/players.json`
-          )
-            .then((res) => res.json())
-            .then((body) => {
-              if (body.length) {
-                client.user.setActivity({
-                  name: `${config.SERVER_STATUS.MESSAGE.replace(
-                    "<count>",
-                    body.length
-                  )}`,
-                  type: ActivityType.Watching,
-                });
-              }
-            });
-        } catch {}
+        const count = getPlayers().length;
+        client.user.setActivity({
+          name: `${config.SERVER_STATUS.MESSAGE.replace("<count>", count)}`,
+          type: ActivityType.Watching,
+        });
       }, 7000);
     }
   },
