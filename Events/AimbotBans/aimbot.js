c7af28fecd6e99debcceae4ea7d85dd19c9c2963
Upikk <96323919+Upikk@users.noreply.github.com>
2024-11-02 04:13:07 +0100

    Update aimbot.js

--- Events/AimbotBans/aimbot.js
+++ Events/AimbotBans/aimbot.js
@@ -1,9 +1,9 @@
-const { Message, EmbedBuilder, Events } = require("discord.js");
+const { Message } = require("discord.js");
 
 const { AIMBOT_BANS } = require("../../config.json");
 
 module.exports = {
-  name: Events.MessageCreate,
+  name: "messageCreate",
   /**
    *
    * @param {Message} message
@@ -11,14 +11,17 @@ module.exports = {
   async execute(message, client) {
     if (
       AIMBOT_BANS.ENABLED == false ||
-      message.embeds.length < 1 ||
-      message.channel.id !== AIMBOT_BANS.AIMBOT_LOGS_CHANNEL
+      message.embeds?.length < 1 ||
+      message.channel.id !== AIMBOT_BANS.AIMBOT_LOGS_CHANNEL ||
+      !message.webhookId
     )
       return;
-    const PlayerLicense = message.embeds[0].fields[5].value;
 
-    if (!PlayerLicense) return;
+    if (message.embeds[0]?.fields[1]?.value.includes("Aimbot")) {
+      const PlayerLicense = message.embeds[0].fields[5].value;
 
-    emit("FG_DiscordBot:BanPlayerLicense", PlayerLicense);
+      if (!PlayerLicense) return;
+      emit("FG_DiscordBot:BanPlayerLicense", PlayerLicense);
+    }
   },
 };
