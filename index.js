70ad10b44559c766f6cf65c97bed7324cc6117e3
Upikk <96323919+Upikk@users.noreply.github.com>
2024-12-27 13:17:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ const { User, Message, GuildMember, ThreadMember, Channel, Reaction } =
 const config = require("./config.json");
 const fs = require("fs");
 const root = GetResourcePath(GetCurrentResourceName());
-const p = require("./version.json");
-const v = p.version;
 
 const fg = exports[config.FIVEGUARD_RESOURCE_NAME];
 
@@ -39,28 +37,6 @@ const client = new Client({
   partials: [User, Message, GuildMember, ThreadMember, Channel, Reaction],
 });
 
-async function checkVer() {
-  const https = require("https");
-  const url =
-    "https://raw.githubusercontent.com/Upikk/Fiveguard_Discordbot/master/version.json";
-  https.get(url, (res) => {
-    let data = "";
-    res.on("data", (chunk) => {
-      data += chunk;
-    });
-    res.on("end", () => {
-      const jsonData = JSON.parse(data);
-      const version = jsonData.version;
-      const changelog = jsonData.changelog;
-      if (version !== v)
-        return console.log(
-          `^3New update is Available!\n\nLatest Version: ${version}\nYour Version: ${v}\n\nChangelog: ${changelog}\n\nIt's recommended to use Latest Version of the Bot^7`
-        );
-      console.log(`You're using latest version: ${version}!`);
-    });
-  });
-}
-
 const { loadEvents } = require(`${root}/Handlers/eventHandler`);
 const { loadCommands } = require(`${root}/Handlers/commandHandler`);
 
@@ -77,7 +53,6 @@ client
     console.log(
       "\n\nMade By upik_\n\n^3If you need help with the bot you can open a ticket here: https://discord.gg/yXBAPzRpz8^7\n\nStarted Successfully\n\n^3If you're looking for a feature-packed server management bot, you can check it out here: https://upikk.tebex.io/package/6578011!\n\n^7"
     );
-    checkVer();
   })
   .catch((err) => {
     console.error(err);
