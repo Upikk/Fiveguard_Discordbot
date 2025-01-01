9487a5df10dbe29ea8de1409efd7e4c7978e3da3
Upikk <96323919+Upikk@users.noreply.github.com>
2025-01-01 20:46:12 +0100

    Update buttonpagination.js

--- Events/ButtonPagination/buttonpagination.js
+++ Events/ButtonPagination/buttonpagination.js
@@ -37,7 +37,7 @@ module.exports = {
 
     const { customId } = interaction;
     const bansjson = JSON.parse(
-      readFileSync(`${GetResourcePath(client.fgName)}/bans.json`, {
+      readFileSync(`${GetResourcePath(client.fiveguardName)}/bans.json`, {
         encoding: "utf8",
       })
     );
