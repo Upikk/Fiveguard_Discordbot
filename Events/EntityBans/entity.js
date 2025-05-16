71ec9ec20e3c8b72450aed074cc1dbf9bc8767e1
Upikk <96323919+Upikk@users.noreply.github.com>
2025-05-16 18:40:38 +0200

    Add files via upload

--- Events/EntityBans/entity.js
+++ Events/EntityBans/entity.js
@@ -10,18 +10,18 @@ module.exports = {
    */
   async execute(message, client) {
     if (
-      AUTO_BANS.AIMBOT.ENABLED == false ||
+      AUTO_BANS.ENTITY.ENABLED == false ||
       message.embeds?.length < 1 ||
-      message.channel.id !== AUTO_BANS.AIMBOT.AIMBOT_LOGS_CHANNEL ||
+      message.channel.id !== AUTO_BANS.ENTITY.ENTITY_LOGS_CHANNEL ||
       !message.webhookId
     )
       return;
 
-    if (message.embeds[0]?.fields[1]?.value.includes("Aimbot")) {
+    if (message.embeds[0]?.fields[1]?.value.includes("illegal way")) {
       const PlayerLicense = message.embeds[0].fields[5].value;
 
       if (!PlayerLicense) return;
-      emit("FG_DiscordBot:BanPlayerLicense", PlayerLicense, "Aimbot Detected");
+      emit("FG_DiscordBot:BanPlayerLicense", PlayerLicense, "Illegal Entity");
     }
   },
 };
